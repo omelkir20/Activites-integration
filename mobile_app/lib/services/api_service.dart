afcb1b234484d516f621990f7b94ecd37ab91928
omelkir20 <omelkirrebei22@gmail.com>
2026-05-05 23:17:23 +0200

    commit 2

--- mobile_app/lib/services/api_service.dart
+++ mobile_app/lib/services/api_service.dart
@@ -4,7 +4,7 @@ import '../models/etudiant.dart';
 
 class ApiService {
   // Remplace par l'IP de ta machine (pas localhost sur un téléphone physique)
-  static const String baseUrl = 'http://192.168.1.X:8080/api/etudiants';
+  static const String baseUrl = 'http://localhost:8080/api/etudiants';
 
   static Future<List<Etudiant>> fetchEtudiants() async {
     final response = await http.get(Uri.parse(baseUrl));
